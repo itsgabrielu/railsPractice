6f6dddccc7487ca01c13b1e9bb94dfd5ce5b7227
Gabriel Lu <gabrielulu@gmail.com>
2018-02-21 13:50:47 +0800

    added second model comments as well as basic http authentication, completed getting started

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'gabriel', password: 'lulu', only: :destroy
+
   def index
     @articles = Article.all
   end
@@ -9,6 +11,11 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -19,6 +26,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
